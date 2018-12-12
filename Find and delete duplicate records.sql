b5dcf70a265223f5620a618964694d741dadbcf4
AleksandrsKudrins <18666264+AleksandrsKudrins@users.noreply.github.com>
2018-12-12 10:50:55 +0200

    Update Find and delete duplicate records.sql

--- Find and delete duplicate records.sql
+++ Find and delete duplicate records.sql
@@ -2,7 +2,7 @@ SELECT *
   FROM table_name A
  WHERE a.ROWID > ANY (SELECT B.ROWID
                         FROM table_name B
-                       WHERE A.msg_id = B.msg_id AND A.msg_id = B.msg_id);
+                       WHERE A.column1 = B.column1 AND A.column2 = B.column2);
 
 DELETE FROM table_name A
       WHERE a.ROWID >
